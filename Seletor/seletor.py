4a420a6b7bea3b60184e04854e4c5375615cac7a
Gabriel F. Gomes <gabrielfalavinhagomes@gmail.com>
2023-06-11 11:31:20 -0300

    Adicionando selecao dos Validadores por peso

--- Seletor/seletor.py
+++ Seletor/seletor.py
@@ -2,12 +2,14 @@ from database import Database
 from sqlite3 import IntegrityError
 import json
 from transacao import Transacao
+import random as rnd
 
 class Validador:
     id: int
     qtd_moeda: int
     qtd_flags: int
     ip: str
+    chance_escolha: float
 
     def __init__(self, id:int=0, qtd_moeda:int=0, qtd_flags:int=0, ip:str="0.0.0.0"):
         self.id = id
@@ -59,6 +61,13 @@ class Validador:
         ret = db.execute(query, param).fetchone()
             
         return Validador(id=ret[0], qtd_moeda=ret[1], qtd_flags=ret[2], ip=ret[3])
+    
+    def isAtivo(self):
+        return True
+    
+    def enviandoTransacao(self, transacao:Transacao):
+        print(self.toJson())
+        return ""
 
 
 class Seletor:
@@ -116,6 +125,37 @@ class Seletor:
             raise Exception("IP ja cadastrado")
         self.validadores.append(validador)
         return validador
+                
+    def buscandoValidadoresAtivos(self) -> list[Validador]:
+        if len(self.validadores) == 0:
+            return []
+        validadores_ativos = []
+        for v in self.validadores:
+            if v.isAtivo():
+                validadores_ativos.append(v)
+        return validadores_ativos
+
+    def enviandoTransacaoValidadores(self, transacao:Transacao):
+        validadores_ativos = self.buscandoValidadoresAtivos()
+        qtd_ativos = len(validadores_ativos)
+        if qtd_ativos < 3:
+            raise Exception("Quantidade insuficiente de validadores")
+        
+        definindoChancesValidadores(validadores_ativos)
+        if qtd_ativos == 3 or qtd_ativos == 5:
+            for v in self.validadores:
+                v.enviandoTransacao(transacao=transacao)
+        elif qtd_ativos < 5:
+            for _ in range(3):
+                v = rnd.choices(population=validadores_ativos, weights=listaChances(validadores_ativos))
+                v[0].enviandoTransacao(transacao=transacao)
+                validadores_ativos.remove(v[0])
+        else:
+            for _ in range(5):
+                v = rnd.choices(population=validadores_ativos, weights=listaChances(validadores_ativos))
+                v[0].enviandoTransacao(transacao=transacao)
+                validadores_ativos.remove(v[0])
+        
 
     def validarTransacao(self, transacao:Transacao):
         # chamada ao validador
@@ -130,3 +170,30 @@ class Seletor:
         except Exception as e:
             print(str(e))
             return 0
+
+
+
+# chamadas aqui em baixo por estarmos usando lista 
+# que nao vem do self do Seletor
+def totalMoedaEntreValidadores(validadores:list[Validador]):
+    total_moeda = 0
+    for v in validadores:
+        total_moeda = total_moeda + v.qtd_moeda
+    return total_moeda
+
+def definindoChancesValidadores(validadores:list[Validador]):
+    total_moeda = totalMoedaEntreValidadores(validadores)
+    for v in validadores:
+        chance = v.qtd_moeda/total_moeda
+        if chance > 0.45:
+            v.chance_escolha = 0.45
+        elif chance < 0.05:
+            v.chance_escolha = 0.05
+        else:
+            v.chance_escolha = chance
+            
+def listaChances(validadores:list[Validador]):
+    chances = []
+    for v in validadores:
+        chances.append(v.chance_escolha)
+    return chances
